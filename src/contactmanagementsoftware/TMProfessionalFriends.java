c7c03b328171a394ce4c33a5c50608216f1c42d1
omarelsharawy <38082286+omarelsharawy@users.noreply.github.com>
2019-12-17 16:44:14 +0800

    Delete TMProfessionalFriends.java

--- src/contactmanagementsoftware/TMProfessionalFriends.java
+++
@@ -1,9 +0,0 @@
-package professionalacquitances;
-
-public abstract class TMProfessionalFriends extends TMCasualFriends {
-    
-    public void information() {
-        System.out.println("Information: UI Developer at IT Solutions");
-    }
-} // Close class
-
